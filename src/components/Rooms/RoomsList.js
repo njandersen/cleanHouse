3bf1eba8d3916096de837e6def19488771986050
njandersen <njandersen22@gmail.com>
2022-10-25 11:39:33 -0400

    minor css change

--- src/components/Rooms/RoomsList.js
+++ src/components/Rooms/RoomsList.js
@@ -3,11 +3,14 @@ import ChoresList from "../Chores/ChoreList";
 
 export default function RoomsList({ rooms }) {
   return (
-    <main>
+    <main className="md:flex md:flex-row md:justify-between">
       {rooms.map((room) => (
-        <div>
-          <h1 key={room.id}>{room.name}</h1>
-          <ul className="md:flex md:flex-row md:justify-between">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white text-black mb-6 mt-8 p-3 w-80">
+          <h1 className="text-3xl p-3 bg-gray-400 " key={room.id}>
+            {room.name}
+          </h1>
+          <hr />
+          <ul>
             {room.chores.map((chore, i) => (
               <li key={i}>{chore.name}</li>
             ))}
